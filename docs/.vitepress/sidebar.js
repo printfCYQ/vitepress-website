dbf77088406aef6a9533877bb6325453febae802
printfCYQ <459987870@qq.com>
2022-11-28 13:54:37 +0800

    dynamic sidebar

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,53 +1,45 @@
+var fs = require("fs");
+
+let nameList = [
+  "javascript/base",
+  "javascript/tools/Array",
+  "javascript/tools/Date",
+];
+let sideBarObject = {};
+for (let name of nameList) {
+  console.log(name);
+  let arr = [];
+  fs.readdir(`./docs/${name}`, (err, files) => {
+    files
+      .filter((i) => i !== "index.md")
+      .map((item) => {
+        arr.push({
+          text: item.split(".md")[0],
+          link: `/${name}/` + item.split(".md")[0],
+        });
+      });
+  });
+  sideBarObject[name] = arr;
+}
+console.log(sideBarObject);
+
 const sidebar = {
   "/javascript/tools": [
     {
       text: "javascript-tools-Array",
-      items: [
-        {
-          text: "01.数组对象按某一项找最大值最小值",
-          link: "/javascript/tools/Array/01.数组对象按某一项找最大值最小值.md",
-        },
-        {
-          text: "02.一维数组转二维数组",
-          link: "/javascript/tools/Array/02.一维数组转二维数组.md",
-        },
-        {
-          text: "03.数组去重",
-          link: "/javascript/tools/Array/03.数组去重.md",
-        },
-      ],
+      items: sideBarObject["javascript/tools/Array"],
+      collapsible: true,
+    },
+    {
+      text: "javascript-tools-Date",
+      items: sideBarObject["javascript/tools/Date"],
       collapsible: true,
     },
   ],
   "/javascript/base": [
     {
       text: "javascript-base",
-      items: [
-        {
-          text: "01.原型-原型链",
-          link: "/javascript/base/01.原型-原型链",
-        },
-        {
-          text: "02.立即执行函数",
-          link: "/javascript/base/02.立即执行函数",
-        },
-        {
-          text: "03.闭包",
-          link: "/javascript/base/03.闭包",
-        },
-        {
-          text: "04.作用域-作用域链",
-          link: "/javascript/base/04.作用域-作用域链",
-        },
-        {
-          text: "05.预编译",
-          link: "/javascript/base/05.预编译",
-        },
-        {
-          text: "06.this-apply-call-bind",
-          link: "/javascript/base/06.this-apply-call-bind",
-        },
-      ],
+      items: sideBarObject["javascript/base"],
       collapsible: true,
     },
   ],
